c2cc1f698ca2122ebc86b7fd7336e34f91714dc0
Evgenii Koriakin <eakoryakin@gmail.com>
2016-06-07 00:22:20 +0400

    Set up Heroku.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname + '/build'));
 
 // Set views directory.
 app.set('views', __dirname + '/build');
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 // Set routes.
 app.get('/', function(request, response) {
